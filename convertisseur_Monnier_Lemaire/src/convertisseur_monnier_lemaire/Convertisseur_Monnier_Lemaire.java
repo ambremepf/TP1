82e360bf438bbffa86a723115774ba73d067e48a
jeanl <jeanl@192.168.115.95>
2021-10-07 10:31:47 +0200

    création du depot

--- convertisseur_Monnier_Lemaire/src/convertisseur_monnier_lemaire/Convertisseur_Monnier_Lemaire.java
+++ convertisseur_Monnier_Lemaire/src/convertisseur_monnier_lemaire/Convertisseur_Monnier_Lemaire.java
@@ -11,7 +11,8 @@ import java.util.Scanner;
 public class Convertisseur_Monnier_Lemaire {
 
     /**
-     * @param args the command line arguments
+     * @param tempCelsius
+     * @return 
      */
     public float CelsiusVersKelvin(float tempCelsius){
         float tempKelvin;
@@ -20,14 +21,38 @@ public class Convertisseur_Monnier_Lemaire {
     }
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
-     System.out.println("Entrer votre température: ");
-     float nb1 = reader.nextFloat();
-     
-     CelsiusVersKelvin(nb1);
-     
-     
-     
-     
+        System.out.println("Entrer votre température: ");
+        float nb1 = reader.nextFloat();
+    }
+    public float KelvinVersCelcius (float tempKelvin){
+        float tempCelcius;
+        tempCelcius=tempKelvin-275;
+        return tempCelcius;
+    }
+    public float FarenheitVersCelcius( float tempFarenheit){
+        float tempCelcius;
+        tempCelcius=(32*tempFarenheit-32)*(5/9);
+        return tempCelcius;
+    }
+    
+    public float CelciusVersFarenheit (float tempCelcius){
+        float tempFarenheit;
+        tempFarenheit = (tempCelcius*(9/5))+32;
+        return tempFarenheit;
+    }
+    
+    public double KelvinVersFarenheit(double tempKelvin){
+        double tempFarenheit;
+        tempFarenheit = (tempKelvin*1.8)-459.67;
+        return tempFarenheit;
+    }
+    // nous sommes passé à des variables de type double car le deboger a trouvé un problème avec le type float.
+    
+    
+    public double FarenheitVersKelvin(double tempFarenheit){
+        double tempKelvin;
+        tempKelvin = (tempFarenheit+459.67)/1.8;
+        return tempKelvin;
     }
     
 }
