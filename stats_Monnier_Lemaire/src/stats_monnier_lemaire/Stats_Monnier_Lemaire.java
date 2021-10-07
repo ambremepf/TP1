176f4aae498037d20b0e8043ba476333e34c0dd7
ambre <ambre@DESKTOP-3BCRSK6>
2021-10-07 23:33:27 +0200

    creation depot

--- stats_Monnier_Lemaire/src/stats_monnier_lemaire/Stats_Monnier_Lemaire.java
+++ stats_Monnier_Lemaire/src/stats_monnier_lemaire/Stats_Monnier_Lemaire.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package stats_monnier_lemaire;
-
+import java.util.Scanner;
+import java.util.Random;
 /**
  *
  * @author ambre
@@ -14,7 +15,45 @@ public class Stats_Monnier_Lemaire {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        float [] tabEntiers = new float[6];
+     for (int i=0;i<6;i++){
+        tabEntiers[i]=0;
+     }
+     Scanner reader = new Scanner(System.in);  // Reading from System.in
+     System.out.println("Entrer un entier: ");
+     int m = reader.nextInt();
+     for (int j=1;j<=m;j++){
+      Random aleatoire =new Random();
+      int n = aleatoire.nextInt(5);
+      if (n==0){
+         tabEntiers[0]=tabEntiers[0]+1;    
+      }
+      if (n==1){
+         tabEntiers[1]=tabEntiers[1]+1;    
+      }
+      if (n==2){
+         tabEntiers[2]=tabEntiers[2]+1;    
+      }
+      if (n==3){
+         tabEntiers[3]=tabEntiers[3]+1;    
+      } 
+      if (n==4){
+         tabEntiers[4]=tabEntiers[4]+1;    
+      }
+      if (n==5){
+         tabEntiers[5]=tabEntiers[5]+1;    
+      }
+     }
+     for(int i=0;i<6;i++){
+     System.out.println(tabEntiers[i]);  
+     }
+     for(int i=0;i<6;i++){
+     System.out.println(((tabEntiers[i])/m)*100+"%");  
+     }
+    }
     }
     
-}
+
+    
+        
+
