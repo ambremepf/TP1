5af6bf9cf91cfd9e0f178fdccff9f772267060f6
ambre <ambre@DESKTOP-3BCRSK6>
2021-10-07 18:12:49 +0200

    creation depot

---
+++ stats_Monnier_Lemaire2/src/stats_monnier_lemaire2/Stats_Monnier_Lemaire2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stats_monnier_lemaire2;
+
+/**
+ *
+ * @author ambre
+ */
+public class Stats_Monnier_Lemaire2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
