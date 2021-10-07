e395cc5c27cef467a300a5bf35f28f19e347d03b
jeanl <jeanl@192.168.114.168>
2021-10-07 17:01:01 +0200

    création du depot

--- guessMyNumber_Monnier_Lemaire/src/guessmynumber_monnier_lemaire/GuessMyNumber_Monnier_Lemaire.java
+++ guessMyNumber_Monnier_Lemaire/src/guessmynumber_monnier_lemaire/GuessMyNumber_Monnier_Lemaire.java
@@ -1,15 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+guessMyNumber/MOnnier Ambre et Lemaire Jean/ 07/10/2021
  */
 package guessmynumber_monnier_lemaire;
 
 import java.util.Random;
-
-/**
- *
- * @author jeanl
- */
+import java.util.Scanner;
 public class GuessMyNumber_Monnier_Lemaire {
 
     /**
@@ -18,14 +13,33 @@ public class GuessMyNumber_Monnier_Lemaire {
     public static void main(String[] args) {
  
      Random generateurAleat = new Random();
-  
+     int rep = 0;
      int n = generateurAleat.nextInt(100);
-     System.out.println(n);
+     int i = 0;
+     Scanner saisieUtilisateur = new Scanner(System.in);
      
+     System.out.println("quel niveau voules-vous jouer ? (1, 2 ou 3)");
+
+     int niv= saisieUtilisateur.nextInt();
      
-       
+     if (niv == 1){
+         while (rep != n){
+        System.out.println("Veuillez entrer un nombre : ");
+        int reputilisateur = saisieUtilisateur.nextInt();
+        rep = reputilisateur;
+        i=i+1;
+        if (rep < n){
+            System.out.println("plus grand");
+        }
+        else {
+            System.out.println("plus petit");
+        }
+         
+     }
+     System.out.println("bonne réponse !");
+     System.out.println("vous avez mis "+i+" tours");
     }
     
+
     
     
-}
