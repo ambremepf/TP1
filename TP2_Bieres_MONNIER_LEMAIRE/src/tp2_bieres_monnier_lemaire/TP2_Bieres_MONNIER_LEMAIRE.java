19f011fa02e727d65213377a59b5a3eac3394c1d
jeanl <jeanl@192.168.114.168>
2021-10-08 10:45:31 +0200

    création de dépot

---
+++ TP2_Bieres_MONNIER_LEMAIRE/src/tp2_bieres_monnier_lemaire/TP2_Bieres_MONNIER_LEMAIRE.java
@@ -0,0 +1,19 @@
+/*
+TP2 bières, Ambre Monnier et Jean Lemaire, 08/10/2021
+ */
+package tp2_bieres_monnier_lemaire;
+
+/**
+ *
+ * @author jeanl
+ */
+public class TP2_Bieres_MONNIER_LEMAIRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
