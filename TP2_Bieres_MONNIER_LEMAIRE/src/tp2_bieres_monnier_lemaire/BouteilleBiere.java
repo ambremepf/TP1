f4bdb1eab7026435c3f74b79f254986ed1e0f066
jeanl <jeanl@192.168.114.67>
2021-10-18 15:56:56 +0200

    fin question 13

--- TP2_Bieres_MONNIER_LEMAIRE/src/tp2_bieres_monnier_lemaire/BouteilleBiere.java
+++ TP2_Bieres_MONNIER_LEMAIRE/src/tp2_bieres_monnier_lemaire/BouteilleBiere.java
@@ -44,5 +44,14 @@ public class BouteilleBiere {
         brasserie = uneBrasserie; 
         ouverte = false; 
     }
+     
+     @Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
 }
 
