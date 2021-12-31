eed03a53dc4b120328d708202ee2374a1f8e99ba
ambre <ambre@DESKTOP-3BCRSK6>
2021-12-31 14:47:22 +0100

    completer des classes

---
+++ mastermind_Monnier8_Lemaire/src/mastermind_monnier8_lemaire/Verification.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_monnier8_lemaire;
+
+/**
+ *
+ * @author ambre
+ */
+public class Verification(int i,int j) {
+    int nbdepionbienplacé=0;
+    ArrayList<String> lignejoueur=newArrayList<String>();
+if ((combinaison[j])==(grilleJeu.Cellule[j]){
+    
+}
+    
+}
