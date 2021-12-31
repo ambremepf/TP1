33d0e7fef3dd25a759f2846d8134b4c4f565f3bd
ambre <ambre@DESKTOP-3BCRSK6>
2021-12-31 15:30:57 +0100

    modif verif class

--- mastermind_Monnier8_Lemaire/src/mastermind_monnier8_lemaire/Verification.java
+++ mastermind_Monnier8_Lemaire/src/mastermind_monnier8_lemaire/Verification.java
@@ -8,11 +8,17 @@ package mastermind_monnier8_lemaire;
  *
  * @author ambre
  */
-public class Verification(int i,int j) {
-    int nbdepionbienplacé=0;
-    ArrayList<String> lignejoueur=newArrayList<String>();
-if ((combinaison[j])==(grilleJeu.Cellule[j]){
-    
+public class Verification{
+    int nbdepionbienplacé=0;    
+    public void vérificationpion(String [] grillejeu,String []grilleordi){
+        for (int j=0;j<=3;j++){
+            if(grilleordi[j]==grillejeu[j]){
+             nbdepionbienplacé=nbdepionbienplacé+1;
+             for ()
+             grilleordi[j]=grilleordi[j+1];
+             
+            }
+        }
+        
 }
-    
 }
